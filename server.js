5be86409d3e32154a91075f48217085fdd61aab2
Andres Root <andres.root.coder@gmail.com>
2015-05-16 16:43:21 -0500

    working on js new linter

--- server.js
+++ server.js
@@ -2,15 +2,28 @@
 var express = require('express');
 var app = express();
 
-// Creating jslint stream
+// Creating ESlint stream
 var LintStream = require('jslint').LintStream;
 var options = {
     "edition": "latest",
     "length": 100
 }
+linter = new LintStream(options);
+
 
 app.get('/', function(req, res){
-  res.send('hello world');
+	var fileName, fileContents;
+	fileContents = req.query.code;
+	linter.write({file: fileName, body: fileContents});
+
+	linter.on('data', function (chunk, encoding, callback) {
+	    assert.deepEqual(chunk.file, fileName);
+	    console.log(chunk.linted	)
+	    errors = chunk.linted
+	});
+	res.send(errors);
+
+
 });
 
 app.listen(3000);
