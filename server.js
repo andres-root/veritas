1181e2a6a1a3a8e3e503cd45a86d95e576128767
Andres Root <andres.root.coder@gmail.com>
2015-05-16 15:13:22 -0500

    adding package.json file

--- server.js
+++ server.js
@@ -2,6 +2,13 @@
 var express = require('express');
 var app = express();
 
+// Creating jslint stream
+var LintStream = require('jslint').LintStream;
+var options = {
+    "edition": "latest",
+    "length": 100
+}
+
 app.get('/', function(req, res){
   res.send('hello world');
 });
