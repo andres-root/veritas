8d53896edb0d960c912f55537ae8ca99453850b9
Carlos Avila <caravila@cobogpgp1107.local>
2015-05-16 23:45:55 -0500

    add user grade data

--- content/js/modules/index.js
+++ content/js/modules/index.js
@@ -5,8 +5,13 @@ define([
 		$,
 		ko
 	){
-
-		var indexData;
+		var indexData,
+			$usersCont = $('.user-list__main-list'),
+			$qualifyBtn = $('.userid'),
+			$finderUsers = $('.user-list'),
+			$qualifySec = $('.section-qualify'),
+			$closeUser = $('.section-qualify__close'),
+			userQualify;
 
 		$.ajax({
 			"url": "../../content/json/index.json",
@@ -14,9 +19,35 @@ define([
 		}).done(function(response){
 			console.log(response);
 			indexData = response;
-			ko.applyBindings(indexData);
+			ko.applyBindings(indexData, $finderUsers[0]);
 		}).fail(function(error){
 			console.log(error);
 		});
 
+		$usersCont.on("click", $qualifyBtn, qualifyUser);
+
+		function qualifyUser() {
+
+			var userId = $(this).attr('data-userid');
+
+			$.ajax({
+				"url": "../../content/json/qualifyUser.json",
+				"method": "GET",
+				"data": {userid: userId}
+			}).done(function(response) {
+				var winHeight = $('body').height();
+				indexData = response;
+				ko.applyBindings(indexData, $qualifySec[0]);
+
+				$qualifySec.css({'height': winHeight});
+			}).fail(function(error) {
+				console.log(error);
+			});
+		}
+
+		$closeUser.on("click", function(){
+			$qualifySec.removeClass('active')
+			ko.cleanNode($qualifySec[0]);
+		});
+	
 });
