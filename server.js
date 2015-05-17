76d6d4236eaad6748a01f5bdf6efc8d24c134f7f
Andres Root <andres.root.coder@gmail.com>
2015-05-16 20:24:30 -0500

    veryfied linder

--- server.js
+++ server.js
@@ -3,27 +3,17 @@ var express = require('express');
 var app = express();
 
 // Creating ESlint stream
-var LintStream = require('jslint').LintStream;
-var options = {
-    "edition": "latest",
-    "length": 100
-}
-linter = new LintStream(options);
-
+var linter = require("eslint").linter;
+var linterConf = { rules: { "semi": 1, "no-empty": 1, "no-empty-class": 1, "no-obj-calls": 1, "no-regex-spaces": 1, "no-undef": 1, "no-undefined": 1, "comma-spacing": 1, "comma-style": 1, "consistent-this": 1 , "camelcase": 1} };
 
 app.get('/', function(req, res){
-	var fileName, fileContents;
-	fileContents = req.query.code;
-	linter.write({file: fileName, body: fileContents});
+	res.send('is working');
+});
 
-	linter.on('data', function (chunk, encoding, callback) {
-	    assert.deepEqual(chunk.file, fileName);
-	    console.log(chunk.linted	)
-	    errors = chunk.linted
-	});
-	res.send(errors);
-
-
+app.get('/code', function(req, res){
+	code = req.query.code;
+	var messages = linter.verify(code, linterConf);
+	res.send(messages);
 });
 
 app.listen(3000);
